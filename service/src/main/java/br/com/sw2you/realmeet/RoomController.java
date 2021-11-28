8623e08210d06e9396a5e7f8e86f8a455d2a99cb
Rodrigo Silvério <rodrigo.silverio@fengbrasil.com.br>
2021-11-28 16:01:48 -0300

    Fix prettier and refactor configs

--- service/src/main/java/br/com/sw2you/realmeet/RoomController.java
+++ service/src/main/java/br/com/sw2you/realmeet/RoomController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RoomController implements RoomsApi {
 
-    @Override
-    public CompletableFuture<ResponseEntity<Room>> listRooms(@Valid Long id) {
-        return supplyAsync(() -> ResponseEntity.ok(new Room().id(1L).name("Room #1")));
-    }
+  @Override
+  public CompletableFuture<ResponseEntity<Room>> listRooms(@Valid Long id) {
+    return supplyAsync(() -> ResponseEntity.ok(new Room().id(1L).name("Room #1")));
+  }
 }
